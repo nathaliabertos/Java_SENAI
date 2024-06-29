d01d8746251100a7c34b82050e7a6d24d42361eb
Nathalia Bertos <156721397+nathaliabertos@users.noreply.github.com>
2024-06-29 14:44:34 -0300

    Add files via upload
    Oracle_Java_Foundations_Aula1

---
+++ Aula_1_Java.java
@@ -0,0 +1,45 @@
+package sp.senai.br.aula_1_java;
+import java.util.Scanner;
+
+public class Aula_1_Java {
+
+    public static void main(String[] args) {
+        /*Scanner scanner = new Scanner(System.in);    
+        System.out.println("Digite seu nome: ");
+        String nome = scanner.nextLine();
+        
+        System.out.println("Digite sua altura: ");
+        String altura = scanner.nextLine();
+        
+        System.out.println("Digite sua idade: ");
+        String idade = scanner.nextLine();
+        
+        System.out.println(nome + " " + altura + " " + idade + "");*/ 
+              
+        /*System.out.println(10+10);
+        System.out.println(10-5);
+        System.out.println(10*2);
+        System.out.println(5/3);
+        System.out.println(5.0/3);
+        System.out.println(5%3);*/
+        
+        /*int numero = 5;
+        System.out.println(numero = numero + 5);*/
+        
+        /*int numero = 5;
+        System.out.println(numero += 5);
+        System.out.println(numero -= 2);
+        System.out.println(numero *= 10);
+        System.out.println(numero /= 4);
+        System.out.println(numero %= 6);
+        
+        System.out.println(++numero);
+        System.out.println(--numero);*/
+        
+        String nome = "Nathalia";
+        String sobrenome = "Bertos";
+        
+        String concatenado = "Meu nome é " + nome + " " + sobrenome;
+        System.out.println(concatenado);
+    }
+}
